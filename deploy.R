3736c6d3258be7816642ca0df4bb49984869def1
lauraselby <31728972+lauraselby@users.noreply.github.com>
2017-11-10 14:55:36 +0000

    Added packrat and travis with basic test (#1)
    * Added packrat
    
    * System dependency
    
    * added v8 dependency
    
    * added deploy script and another dependency
    
    * installed rsconnect package
    
    * another dependency
    
    * another one
    
    * Update .travis.yml
    
    Added missing install to travis.yml
    
    * corrected test case issue
    
    * corrected barchart issue on front page
    
    * fix issue with front page plot
    
    * tidy code

---
+++ deploy.R
@@ -0,0 +1,7 @@
+rsconnect::setAccountInfo(
+  name="department-for-education",
+  token=Sys.getenv("SHINYAPPS_TOKEN"),
+  secret=Sys.getenv("SHINYAPPS_SECRET")
+)
+
+rsconnect::deployApp()
---
+++ test.R
@@ -0,0 +1,1 @@
+source("tests/test-connection.R")
